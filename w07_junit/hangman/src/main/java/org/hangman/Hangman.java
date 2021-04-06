8bca7f8598f6cfec889c375942a120605ac40213
reimoramman <reimoramman@online.ee>
2021-04-06 20:15:35 +0300

    Algus

---
+++ w07_junit/hangman/src/main/java/org/hangman/Hangman.java
@@ -0,0 +1,14 @@
+/*Soovitavalt 3 liikmelise võistkonnana (võib 2 ka)
+teha mingisugune mäng (sõnamäng, trips-traps-trull),
+kus oleksid olemas interface, testid, mänguloogika 
+(kus on implementitud interface) ja mängu 
+realiseeriv rakendus (mingisugune klass,
+kus main meetod, kus te teete uue objekti mängust,
+kus kasutaja saab mängida läbi)*/
+
+
+package org.hangman;
+
+public class Hangman{
+   private static String[] words = {"apple", "banana", "programming", "thunderstorm"};
+}
