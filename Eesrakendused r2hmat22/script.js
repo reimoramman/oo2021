ade4004cc0561ae20f68c43df22254503af4c389
reimoramman <reimoramman@online.ee>
2021-04-22 17:14:52 +0300

    Eesrakendused r2hmat22

---
+++ Eesrakendused r2hmat22/script.js
@@ -0,0 +1,25 @@
+/*function avalehele(){
+    location.replace("Sisesta korrektne URL");
+}
+
+function kontaktlehele(){
+location.replace("Sisesta korrektne URL");
+}*/
+
+// $(document).ready(function() { 
+ 
+//     $('#btn-submit').click(function() {  
+ 
+//         $(".error").hide();
+//         var hasError = false;
+ 
+//         var emailaddressVal = $("#UserEmail").val();
+//         if(emailaddressVal == '') {
+//             $("#UserEmail").after('<span class="error">Please enter your email address.</span>');
+//             hasError = true;
+//         }
+ 
+//         if(hasError == true) { return false; }
+ 
+//     });
+// });
