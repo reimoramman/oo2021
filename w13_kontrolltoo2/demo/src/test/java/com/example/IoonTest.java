a80902a137e1c1c22d28273293050a0af6418bf7
reimoramman <reimoramman@online.ee>
2021-05-07 15:26:57 +0300

    KT2 ei ole veel valmis

--- w13_kontrolltoo2/demo/src/test/java/com/example/IoonTest.java
+++ w13_kontrolltoo2/demo/src/test/java/com/example/IoonTest.java
@@ -7,6 +7,11 @@ import org.junit.Test;
 public class IoonTest{
     Ioon ioon1 = new IoonInfo("H+", 1, 1);
     Ioon ioon2 = new IoonInfo("NO3-", 62.0049, -1);
-    
+    Aine aine = new LisaAine(null, 0);
+
+    @Test
+    public void test1(){
+        assertEquals("H+", ioon1.getNimetus());
+    }
 
 }
