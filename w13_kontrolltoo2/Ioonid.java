1d75168f13c0a4a8e4c4c8deb007ceea4eb86e33
reimoramman <reimoramman@online.ee>
2021-05-07 13:26:46 +0300

    KT2

---
+++ w13_kontrolltoo2/Ioonid.java
@@ -0,0 +1,9 @@
+public interface Ioonid{
+  String nimetus = "nimi";
+  double aatomMassideSumma = 0;
+  float laeng = 0;
+
+  public String nimetus();
+  public double aatomMassideSumma();
+  public float laeng();
+}
---
+++ w13_kontrolltoo2/IoonidTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class IoonidTest {
+  
+  @Test
+}
+d
