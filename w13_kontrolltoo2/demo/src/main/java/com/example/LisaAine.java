7cbfdb07148ee98d601a7e272d3cc785a8937d3e
reimoramman <reimoramman@online.ee>
2021-05-07 15:02:04 +0300

    KT2

--- w13_kontrolltoo2/demo/src/main/java/com/example/LisaAine.java
+++ w13_kontrolltoo2/demo/src/main/java/com/example/LisaAine.java
@@ -13,13 +13,24 @@ public class LisaAine implements Aine {
   public String getNimetus(Ioon ioon1, Ioon ioon2) {
     // TODO Auto-generated method stub
     //return null;
-    
+    if(ioon1.getLaeng() + ioon2.getLaeng() == 0){
+      if(ioon1.getLaeng() > 0){
+        nimetus = ioon1.getNimetus() + ioon2.getNimetus();
+      }
+    }else{
+      nimetus = ioon2.getNimetus() + ioon1.getNimetus();
+    }
+    return nimetus;
   }
 
   @Override
   public double getAatomMassideSumma(Ioon ioon1, Ioon ioon2) {
     // TODO Auto-generated method stub
-    return 0;
+    //return 0;
+    if(ioon1.getLaeng() + ioon2.getLaeng() == 0){
+      aatomMassideSumma = ioon1.getAatomMassideSumma() + ioon2.getAatomMassideSumma();
+    }
+    return aatomMassideSumma;
   }
 
   
